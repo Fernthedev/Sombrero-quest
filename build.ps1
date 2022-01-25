2dbed3a352652277c135bfcf3f81d8d76a5ece84
RedBrumbler <51795403+RedBrumbler@users.noreply.github.com>
2022-01-25 13:24:49 +0100

    Redirect files, constexpr and concepts (#7)
    * Add redirect files because filenames are confusing, added implicit conversion using concepts
    
    * Power now handles negative numbers
    
    * skill issue, it was actually wrong and I just happened to pick correct examples so that the wrongness didn't appear
    
    * Accidentally removed codegen skill issue

--- build.ps1
+++ build.ps1
@@ -1,10 +1,6 @@
 $NDKPath = Get-Content $PSScriptRoot/ndkpath.txt
 
-$buildScript = "$NDKPath/build/ndk-build"
-if (-not ($PSVersionTable.PSEdition -eq "Core")) {
-    $buildScript += ".cmd"
-}
-
+$buildScript = "$NDKPath/build/ndk-build.cmd"
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
 
 Exit $LASTEXITCODE
--- src/test.cpp
+++ src/test.cpp
@@ -15,4 +15,36 @@ int main() {
     Sombrero::vector2Str(vec2);
 
     Sombrero::HSBColor();
+
+    // test concepts to see if we are allowed to assign a vector3 to a color
+
+    static_assert(Sombrero::Clamp01(2.0f) == 1.0f);
+    static_assert(Sombrero::Clamp01(0.8f) == 0.8f);
+    static_assert(Sombrero::Clamp01(-1.2f) == 0.0f);
+
+#ifdef USE_SOMBRERO_IMPLICIT_CONVERSIONS
+    Sombrero::FastColor color = vec3;
+    Sombrero::FastVector2 vec2_2 = color;
+
+    Sombrero::Lerp(vec2, vec2_2, 0.8f);
+
+    Sombrero::FastVector3 vec3_2;
+    Sombrero::FastQuaternion quat;
+    Sombrero::FastQuaternion quat2;
+    Sombrero::Lerp(quat, quat2, 0.5f);
+
+    vec3_2 += vec3_2;
+
+    vec3_2 += 1.0f;
+    vec3_2 += quat;
+    vec3_2 += vec2;
+#endif
+    constexpr float val = Sombrero::power(2.0f, 2);
+    constexpr float val2 = Sombrero::power(2.0f, -1);
+    constexpr float val3 = Sombrero::power(3, -2);
+    constexpr float val4 = Sombrero::power(5, -3);
+    static_assert(val == 4.0f);
+    static_assert(val2 == 0.5f);
+    static_assert(val3 == 1.0f / 9.0f);
+    static_assert(val4 == 1.0f / 125.0f);
 }
